9d26e29c3ca03e0b97aa91b4afd560304df9b92b
Bob Payne <bob@electroglide.biz>
2010-07-30 12:04:38 -0400

    fixed broken links

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,7 @@ Rails::Initializer.run do |config|
   config.gem "browsercms"
   config.gem "bcms_news"
   config.gem "bcms_blog"
+  config.gem "bcms_feeds"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
